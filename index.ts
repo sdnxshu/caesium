535f936ad727980cece940be67a75325ea15b380
sdnxshu <contact@sudhanshuneravati.com>
2025-11-06 19:40:41 +0530

    feat(auth): initialize better-auth

--- index.ts
+++ index.ts
@@ -1,9 +1,34 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
+import { poweredBy } from 'hono/powered-by'
+
+import { auth } from '@/lib/auth';
 
 const app = new Hono()
 
-app.get('/', (c) => {
-    return c.text('Hello Hono!')
-})
+app.use(logger())
+app.use(poweredBy())
 
-export default app
+app.use(
+    "/api/auth/*",
+    cors({
+        origin: "http://localhost:3001",
+        allowHeaders: ["Content-Type", "Authorization"],
+        allowMethods: ["POST", "GET", "OPTIONS"],
+        exposeHeaders: ["Content-Length"],
+        maxAge: 600,
+        credentials: true,
+    }),
+);
+
+app.get('/', (c) => { return c.text('Hello Hono!') })
+app.get('/health', (c) => { return c.text('OK') })
+app.get('/metrics', (c) => { return c.text('metrics') })
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
+
+export default {
+    fetch: app.fetch,
+    port: 4444,
+}
---
+++ lib/auth-client.ts
@@ -0,0 +1,7 @@
+import { createAuthClient } from "better-auth/react"
+
+export const authClient = createAuthClient({
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:4444"
+})
+
+export const { signIn, signUp, useSession } = createAuthClient()
